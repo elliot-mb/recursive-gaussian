e395c85df7a3c98b1b1d65360ead005bd70c83d7
elliotmb <elliotbuckingham14@gmail.com>
2022-02-24 01:15:37 +0000

    works properly now

--- Runner.java
+++ Runner.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 public class Runner {
     public static void main(String[] args){
         Matrix A = new Matrix(new ArrayList<Double>(Arrays.asList( //enter GE matrix as a flat array
-                -2d, 2d, 4d, 5d, 2d,
-                2d, -1d, -3d, 6d, 3d,
-                4d, -2d, -4d, 1d, 8d,
-                1d, 2d, 2d, -2d, -4d
+                1d, -2d,  0d, -1d, 1d,
+                4d,  1d,  1d,  3d, 5d,
+                4d,  2d, -2d,  5d, 2d,
+                6d, -3d,  4d,  3d, 4d
         )));
         System.out.println(A);
 
